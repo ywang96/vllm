1dc9b6da34316d89159719beef748d3ee92f3354
Roger Wang <ywang@roblox.com>
2024-03-29 21:59:11 -0700

    add code

--- benchmarks/benchmark_serving.py
+++ benchmarks/benchmark_serving.py
@@ -169,6 +169,22 @@ def sample_sonnet_requests(
     return sampled_requests
 
 
+def sample_code_requests(
+    dataset_path: str,
+    num_requests: int,
+    tokenizer: PreTrainedTokenizerBase,
+) -> List[Tuple[str, str, int, int]]:
+
+    f = open(dataset_path, "r")
+    code_prompts = json.load(f)
+
+    user_prompts = random.choices(code_prompts, k=num_requests)
+    sampled_requests = []
+    for i in range(num_requests):
+        prompt_len = len(tokenizer(user_prompts[i]).input_ids)
+        sampled_requests.append((user_prompts[i], prompt_len, 150))
+
+
 async def get_request(
     input_requests: List[Tuple[str, int, int]],
     request_rate: float,
@@ -381,6 +397,7 @@ def main(args: argparse.Namespace):
             input_requests = [(prompt, prompt_len, output_len)
                               for prompt, prompt_formatted, prompt_len,
                               output_len in input_requests]
+            
         else:
             assert (
                 tokenizer.chat_template or tokenizer.default_chat_template
@@ -396,6 +413,13 @@ def main(args: argparse.Namespace):
             input_requests = [(prompt_formatted, prompt_len, output_len)
                               for prompt, prompt_formatted, prompt_len,
                               output_len in input_requests]
+
+    elif args.dataset_name == "code":
+        input_requests = sample_code_requests(
+            dataset_path=args.dataset_path,
+            num_requests=args.num_prompts,
+            tokenizer=tokenizer,
+        )
 
     else:
         raise ValueError(f"Unknown dataset: {args.dataset_name}")
@@ -488,7 +512,7 @@ if __name__ == "__main__":
         "--dataset-name",
         type=str,
         default="sharegpt",
-        choices=["sharegpt", "sonnet"],
+        choices=["sharegpt", "sonnet", "code"],
         help="Name of the dataset to benchmark on.",
     )
     parser.add_argument("--dataset-path",
